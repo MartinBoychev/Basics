9e9380555186620c0781cc2bfe4eed97c9457cd4
MartinBoychev <boycheff88@gmail.com>
2022-10-31 23:36:09 +0200

    sync

--- time_plus_15_min.py
+++ time_plus_15_min.py
@@ -1,25 +1,25 @@
-time_in_hours = int(input())
-time_in_minutes = int(input())
+hours = int(input())
+minutes = int(input())
 
-minutes_above = 15 - (60 - time_in_minutes)
+minutes_above = 15 - (60 - minutes)
 
-if -1 < time_in_minutes < 45:
-    print(f"{time_in_hours}:{time_in_minutes + 15}")
-elif time_in_minutes == 45 and time_in_hours == 23:
+if -1 < minutes < 45:
+    print(f"{hours}:{minutes + 15}")
+elif minutes == 45 and hours == 23:
     print("0:00")
-elif 60 > time_in_minutes > 55 and time_in_hours == 23:
+elif 60 > minutes > 55 and hours == 23:
     print(f"0:{minutes_above}")
-elif 55 > time_in_minutes > 45 and time_in_hours == 23:
+elif 55 > minutes > 45 and hours == 23:
     print(f"0:0{minutes_above}")
-elif time_in_minutes == 45:
-    print(f"{time_in_hours + 1}:00")
-elif 51 > time_in_minutes > 45:
-    print(f"{time_in_hours + 1}:0{minutes_above}")
-elif time_in_hours == 23 and 60 > time_in_minutes > 45:
+elif minutes == 45:
+    print(f"{hours + 1}:00")
+elif 51 > minutes > 45:
+    print(f"{hours + 1}:0{minutes_above}")
+elif hours == 23 and 60 > minutes > 45:
     print(f"0:{minutes_above}")
-elif 60 > time_in_minutes > 50:
-    print(f"{time_in_hours + 1}:{minutes_above}")
-elif 50 > time_in_minutes > 45:
-    print(f"{time_in_hours + 1}:0{minutes_above}")
+elif 60 > minutes > 50:
+    print(f"{hours + 1}:{minutes_above}")
+elif 50 > minutes > 45:
+    print(f"{hours + 1}:0{minutes_above}")
 else:
     print("Enter valid hour and minutes")
