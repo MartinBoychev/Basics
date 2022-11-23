94176f8e3d230168545342d4b9e075788e9c3523
MartinBoychev <boycheff88@gmail.com>
2022-11-23 12:17:17 +0200

    new task

---
+++ for_loop/game_of_intervals.py
@@ -0,0 +1,41 @@
+turns = int(input())
+invalid = 0
+zero_to_9 = 0
+ten_to_19 = 0
+twenty_to_29 = 0
+thirty_to_39 = 0
+forty_to_49 = 0
+
+result = 0
+
+for _ in range(0, turns):
+    number = int(input())
+    if 50 < number:
+        invalid += 1
+        result /= 2
+    elif 0 > number:
+        invalid += 1
+        result /= 2
+    elif number < 10:
+        zero_to_9 += 1
+        result = (0.2 * number) + result
+    elif number < 20:
+        ten_to_19 += 1
+        result = (0.3 * number) + result
+    elif number < 30:
+        twenty_to_29 += 1
+        result = (0.4 * number) + result
+    elif number < 40:
+        thirty_to_39 += 1
+        result += 50
+    elif number <= 50:
+        forty_to_49 += 1
+        result += 100
+
+print(f"{result:.2f}")
+print(f"From 0 to 9: {zero_to_9 / turns * 100:.2f}%")
+print(f"From 10 to 19: {ten_to_19 / turns * 100:.2f}%")
+print(f"From 20 to 29: {twenty_to_29 / turns * 100:.2f}%")
+print(f"From 30 to 39: {thirty_to_39 / turns * 100:.2f}%")
+print(f"From 40 to 50: {forty_to_49 / turns * 100:.2f}%")
+print(f"Invalid numbers: {invalid / turns * 100:.2f}%")
