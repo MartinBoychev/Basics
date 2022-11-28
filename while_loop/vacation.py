d4d15f75981fea67fb0400726f4b41fb3da6e30e
MartinBoychev <boycheff88@gmail.com>
2022-11-28 22:03:13 +0200

    new task

--- while_loop/vacation.py
+++ while_loop/vacation.py
@@ -9,19 +9,18 @@ total_sum = amount_money
 while total_sum < excursion_price and spending_days < 5:
     action = input()
     amount_action = float(input())
-
+    total_days += 1
     if action == "spend":
         spending_days += 1
         if amount_action >= total_sum:
             total_sum = 0
         else:
             total_sum -= amount_action
-        total_days += 1
+
     elif action == "save":
         saving_days += 1
         total_sum += amount_action
         spending_days = 0
-        total_days += 1
 
 if total_sum >= excursion_price:
     print(f"You saved the money for {total_days} days.")
