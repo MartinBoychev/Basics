70b58cbedb45339089cd2d9fe40db9855f53a946
MartinBoychev <boycheff88@gmail.com>
2022-11-30 19:43:38 +0200

    new task

--- while_loop/moving.py
+++ while_loop/moving.py
@@ -5,7 +5,7 @@ height_room = int(input())
 full_capacity_room = width_room * length_room * height_room
 boxes = ""
 capacity_left = full_capacity_room
-
+no_more_capacity = False
 while boxes != "Done":
     boxes = input()
     if boxes == "Done":
@@ -13,9 +13,10 @@ while boxes != "Done":
     capacity_left -= int(boxes)
 
     if capacity_left <= 0:
+        no_more_capacity = True
         break
 
-if capacity_left < 0:
+if no_more_capacity:
     print(f"No more free space! You need {-capacity_left} Cubic meters more.")
 else:
     print(f"{capacity_left} Cubic meters left.")
