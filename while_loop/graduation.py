ae43fa0dc257d20ef28de36a9b8b02ae69f87b46
MartinBoychev <boycheff88@gmail.com>
2022-12-01 23:13:11 +0200

    new task

---
+++ while_loop/graduation.py
@@ -0,0 +1,20 @@
+name = input()
+year = 0
+total_grade = 0
+fails = 0
+
+while True:
+    grade = float(input())
+    year += 1
+    if grade < 4:
+        fails += 1
+        if fails == 2:
+            print(f"{name} has been excluded at {year} grade")
+        year -= 1
+    else:
+        total_grade += grade
+
+    if year == 12:
+        avg = total_grade / 12
+        print(f"{name} graduated. Average grade: {avg:.2f}")
+        break
