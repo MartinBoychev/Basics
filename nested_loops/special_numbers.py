e1809cf2642451b3dd89ac459751f17b80c9bfb8
MartinBoychev <boycheff88@gmail.com>
2022-12-07 21:37:57 +0200

    new task

--- nested_loops/special_numbers.py
+++ nested_loops/special_numbers.py
@@ -1,5 +1,8 @@
 n = int(input())
 
-for i in range(1111, 10000):
-    if i % n == 0:
-        print(i)
+for i in range(1, 10):
+    for u in range(1, 10):
+        for y in range(1, 10):
+            for t in range(1, 10):
+                if n % i == 0 and n % u == 0 and n % y == 0 and n % t == 0:
+                    print(f"{i}{u}{y}{t}", end=" ")
